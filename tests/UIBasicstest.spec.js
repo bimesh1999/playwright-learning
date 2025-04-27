6056aadab3047675de9511c01e270bb5c78a65ba
Shrajen <bimeshrajbanshi1999@gmail.com>
2025-04-27 16:15:40 +0545

    Resolved merge conflict

---
+++ tests/UIBasicstest.spec.js
@@ -0,0 +1,42 @@
+const {test} = require('@playwright/test');
+const {expect} = require('@playwright/test');
+
+// test('Browser Context Playwright test',async ({browser})=>
+// {
+//     const context = await browser.newContext();
+//     const page = await context.newPage();
+//     await page.goto("https://www.google.com/");
+// });
+
+
+// test.only('First Playwright test',async ({page})=>
+//     {
+//         // const context = await browser.newContext();
+//         // const page = await context.newPage();
+//         await page.goto("https://www.google.com/");
+//         console.log(await page.title());
+//         await expect(page).toHaveTitle("Google");
+
+
+//     })
+
+test.only('chatapp login', async ({page})=>{
+    const userName = page.locator('#username');
+    const signIn = page.locator('#signInBtn');
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    await userName.fill("rahalshettyacademy");
+    await page.locator('[type="password"]').fill("learning");
+    await signIn.click();
+    console.log(await page.locator("[style*='block']").textContent());
+    await expect(page.locator("[style*='block']")).toContainText("Incorrect username/password");
+    //file
+    await userName.fill("");
+    await userName.fill("rahalshettyacademy");
+    await signIn.click();
+    
+
+
+})
+
+
+
